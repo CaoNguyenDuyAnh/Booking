27997695edec95aea2c8748a770865eda86896cd
DuyAnh <anhduynguyencao@gmail.com>
2023-11-15 16:09:04 +0700

    website booking hotel

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modal/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modal/LoginModal'
+import getCurrentUser from './action/getCurrentUser'
+import RentModal from './components/modal/RentModal'
+import SearchModal from './components/modal/SearchModal'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"]
+})
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <SearchModal/>
+          <RentModal/>
+          <LoginModal/>
+          <RegisterModal/>
+          <Navbar currentUser={currentUser}/>
+        </ClientOnly>
+        <div className='pb-20'>
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
